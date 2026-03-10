4da4ce24756c360de5aaa84878c9045428a594ba
Pragadeesh Rajendran <pragadeesh6@gmail.com>
2026-03-10 19:41:35 +0800

    terraform pipeline

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "terraform-devops-rg"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "tfdevopsstorage123"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
